e96821e1c3d01d79642a7c7447219c146165fbc5
Zeusss <98632598+Zeusyf@users.noreply.github.com>
2022-05-10 07:02:32 +0700

    Update excavator.py

--- nuxhash/miners/excavator.py
+++ nuxhash/miners/excavator.py
@@ -10,25 +10,9 @@ from nuxhash.utils import get_port
 
 
 ALGORITHMS = [
-    'equihash',
-    'pascal',
-    'decred',
-    #'sia',
-    #'lbry',
-    'blake2s',
     'daggerhashimoto',
-    'lyra2rev2',
-    'daggerhashimoto_decred',
-    #'daggerhashimoto_sia',
-    'daggerhashimoto_pascal',
-    #'cryptonight',
-    'keccak',
-    'neoscrypt',
-    #'nist5',
     'cryptonightV7',
-    'cryptonightV8',
-    'lyra2z',
-    'x16r'
+    'cryptonightV8'
     ]
 NHMP_PORT = 3200
 
@@ -387,4 +371,3 @@ class Excavator(miner.Miner):
     def settings(self, v):
         miner.Miner.settings.setter(v)
         self.server.settings = v
-
